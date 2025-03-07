33fdb2b5286b00a1772020786d879fa37a9c1524
Nihar Patel <nihar.patel@powerhrg.com>
2025-03-06 22:20:34 -0500

    script.js functions addedfor image messages

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const image1 = document.getElementById("image1")
+
+image1.addEventListener(click, function () {
+    alert(`Shadowing in Atlanta was one of the best experiences for my role at Power. As a Cost Analyst, I handle
+    cost entries and managing margins across multiple territories, including Atlanta. Getting to meet the
+    people behind the emails and Connect messages, visiting an install, and spending time with the
+    Installers that I process payments for really gave me a clearer picture of how my work ties into the
+    RIO's operations and how our roles coorelate.`)
+})
+
+const image2 = document.getElementById("image2")
+
+image2.addEventListener(click, function () {
+    alert(`Emerge 2024 was hands down one of the best Power Events I’ve been a part of. I met so
+    many awesome people who are now good friends, and the whole experience was both eye-opening with all the deep
+    conversations we had and a ton of fun with all the group activities.`)
+})
+
+const image4 = document.getElementById("image4")
+
+image4.addEventListener(click, function () {
+    alert(`Emerge 2024 was hands down one of the best Power Events I’ve been a part of. I met so
+    many awesome people who are now good friends, and the whole experience was both eye-opening with all the deep
+    conversations we had and a ton of fun with all the group activities.`)
+})
+
+const image5 = document.getElementById("image4")
+
+image5.addEventListener(click, function () {
+    alert(`I had a pretty colorful childhood. I was born in India on May 2, 1999, and lived there
+    until January 21, 2007—the day we moved to the U.S. Growing up in India felt different because I was
+    always surrounded by friends, family, and a culture that my family had known for generations. Moving to a whole new country
+    as a kid was a huge culture shock! Everything felt unfamiliar, I didn't know anyone, and it took time to
+    get properly adjusted.`)
+})
+
 
